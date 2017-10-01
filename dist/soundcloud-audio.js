06f7f31131201a915ec242adf1e1da4781a8434a
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2017-10-01 02:07:46 +0200

    build

--- dist/soundcloud-audio.js
+++ dist/soundcloud-audio.js
@@ -1,236 +1,251 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SoundCloudAudio = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.SoundCloudAudio=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"/Users/dmitri/github/soundcloud-html5-audio":[function(require,module,exports){
 'use strict';
 
 var anchor;
 var keys = 'protocol hostname host pathname port search hash href'.split(' ');
 function _parseURL (url) {
-    if (!anchor) {
-        anchor = document.createElement('a');
-    }
-    anchor.href = url || '';
-    var result = {}
-    for (var i = 0, len = keys.length; i < len; i++) {
-        var key = keys[i];
-        result[key] = anchor[key];
-    }
-    return result;
+  if (!anchor) {
+    anchor = document.createElement('a');
+  }
+
+  var result = {};
+
+  anchor.href = url || '';
+
+  for (var i = 0, len = keys.length; i < len; i++) {
+    var key = keys[i];
+    result[key] = anchor[key];
+  }
+
+  return result;
 }
 
 function _appendQueryParam (url, param, value) {
-    var U = _parseURL(url);
-    var regex = /\?(?:.*)$/;
-    var chr = regex.test(U.search) ? '&' : '?';
-    var result = U.protocol + '//' +  U.host + U.port + U.pathname + U.search + chr + param + '=' + value + U.hash;
-    return result;
+  var U = _parseURL(url);
+  var regex = /\?(?:.*)$/;
+  var chr = regex.test(U.search) ? '&' : '?';
+  var result = U.protocol + '//' +  U.host + U.port + U.pathname + U.search + chr + param + '=' + value + U.hash;
+
+  return result;
 }
 
 function SoundCloud (clientId) {
-    if (!(this instanceof SoundCloud)) {
-        return new SoundCloud(clientId);
-    }
+  if (!(this instanceof SoundCloud)) {
+    return new SoundCloud(clientId);
+  }
 
-    if (!clientId) {
-        throw new Error('SoundCloud API clientId is required, get it - https://developers.soundcloud.com/');
-    }
+  if (!clientId) {
+    console.info('SoundCloud API requires clientId, get it at https://developers.soundcloud.com');
+  }
 
-    this._events = {};
+  this._events = {};
 
-    this._clientId = clientId;
-    this._baseUrl = 'https://api.soundcloud.com';
+  this._clientId = clientId;
+  this._baseUrl = 'https://api.soundcloud.com';
 
-    this.playing = false;
-    this.duration = 0;
+  this.playing = false;
+  this.duration = 0;
 
-    this.audio = document.createElement('audio');
+  this.audio = document.createElement('audio');
 }
 
 SoundCloud.prototype.resolve = function (url, callback) {
-    if (!url) {
-        throw new Error('SoundCloud track or playlist url is required');
+  var resolveUrl = this._baseUrl + '/resolve.json?url=' + encodeURIComponent(url) + '&client_id=' + this._clientId;
+
+  this._json(resolveUrl, function (data) {
+    this.cleanData();
+
+    if (Array.isArray(data)) {
+      var tracks = data;
+      data = {tracks: tracks};
+      this._playlist = data;
+    } else if (data.tracks) {
+      this._playlist = data;
+    } else {
+      this._track = data;
+
+      // save timings
+      var U = _parseURL(url);
+      this._track.stream_url += U.hash;
     }
 
-    var resolveUrl = this._baseUrl + '/resolve.json?url=' + encodeURIComponent(url) + '&client_id=' + this._clientId;
-    this._json(resolveUrl, function (data) {
-        this.cleanData();
+    this.duration = data.duration && !isNaN(data.duration) ?
+      data.duration / 1000 : // convert to seconds
+      0; // no duration is zero
 
-        if (Array.isArray(data)) {
-            var tracks = data;
-            data = {tracks: tracks};
-            this._playlist = data;
-        } else if (data.tracks) {
-            this._playlist = data;
-        } else {
-            this._track = data;
-
-            // save timings
-            var U = _parseURL(url);
-            this._track.stream_url += U.hash;
-        }
-
-        this.duration = data.duration && !isNaN(data.duration) ?
-            data.duration / 1000 : // convert to seconds
-            0; // no duration is zero
-
-        callback(data);
-    }.bind(this));
+    callback(data);
+  }.bind(this));
 };
 
 // deprecated
 SoundCloud.prototype._jsonp = function (url, callback) {
-    var target = document.getElementsByTagName('script')[0] || document.head;
-    var script = document.createElement('script');
+  var target = document.getElementsByTagName('script')[0] || document.head;
+  var script = document.createElement('script');
+  var id = 'jsonp_callback_' + (new Date()).valueOf() + Math.floor(Math.random() * 1000);
 
-    var id = 'jsonp_callback_' + (new Date()).valueOf() + Math.floor(Math.random() * 1000);
-    window[id] = function (data) {
-        if (script.parentNode) {
-            script.parentNode.removeChild(script);
-        }
-        window[id] = function () {};
-        callback(data);
-    };
+  window[id] = function (data) {
+    if (script.parentNode) {
+      script.parentNode.removeChild(script);
+    }
+    window[id] = function () {};
+    callback(data);
+  };
 
-    script.src = _appendQueryParam(url, 'callback', id);
-    target.parentNode.insertBefore(script, target);
+  script.src = _appendQueryParam(url, 'callback', id);
+  target.parentNode.insertBefore(script, target);
 };
 
 SoundCloud.prototype._json = function (url, callback) {
   var xhr = new XMLHttpRequest();
+
   xhr.open('GET', url);
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         var resp = {};
         try {
-            resp = JSON.parse(xhr.responseText);
+          resp = JSON.parse(xhr.responseText);
         } catch (err) {
-            // fail silently
+          // fail silently
         }
         callback(resp);
       }
     }
   };
+
   xhr.send(null);
 };
 
 SoundCloud.prototype.on = function (e, fn) {
-    this._events[e] = fn;
-    this.audio.addEventListener(e, fn, false);
+  this._events[e] = fn;
+  this.audio.addEventListener(e, fn, false);
 };
 
 SoundCloud.prototype.off = function (e, fn) {
-    this._events[e] = null;
-    this.audio.removeEventListener(e, fn);
+  this._events[e] = null;
+  this.audio.removeEventListener(e, fn);
 };
 
 SoundCloud.prototype.unbindAll = function () {
-    for (var e in this._events) {
-        var fn = this._events[e];
-        if (fn) {
-            this.off(e, fn);
-        }
+  for (var e in this._events) {
+    var fn = this._events[e];
+    if (fn) {
+      this.off(e, fn);
     }
+  }
 };
 
 SoundCloud.prototype.preload = function (streamUrl) {
-    this._track = {stream_url: streamUrl};
-    this.audio.src = _appendQueryParam(streamUrl, 'client_id', this._clientId);
+  this._track = {stream_url: streamUrl};
+  this.audio.src = this._clientId ?
+    _appendQueryParam(streamUrl, 'client_id', this._clientId) :
+    streamUrl;
 };
 
 SoundCloud.prototype.play = function (options) {
-    options = options || {};
-    var src;
+  options = options || {};
+  var src;
 
-    if (options.streamUrl) {
-        src = options.streamUrl;
-    } else if (this._playlist) {
-        var length = this._playlist.tracks.length;
-        if (length) {
-            this._playlistIndex = options.playlistIndex || 0;
+  if (options.streamUrl) {
+    src = options.streamUrl;
+  } else if (this._playlist) {
+    var length = this._playlist.tracks.length;
+    if (length) {
+      this._playlistIndex = options.playlistIndex || 0;
 
-            // be silent if index is out of range
-            if (this._playlistIndex >= length || this._playlistIndex < 0) {
-                this._playlistIndex = 0;
-                return;
-            }
-            src = this._playlist.tracks[this._playlistIndex].stream_url;
-        }
-    } else if (this._track) {
-        src = this._track.stream_url;
+      // be silent if index is out of range
+      if (this._playlistIndex >= length || this._playlistIndex < 0) {
+        this._playlistIndex = 0;
+        return;
+      }
+      src = this._playlist.tracks[this._playlistIndex].stream_url;
     }
+  } else if (this._track) {
+    src = this._track.stream_url;
+  }
 
-    if (!src) {
-        throw new Error('There is no tracks to play, use `streamUrl` option or `load` method');
-    }
+  if (!src) {
+    throw new Error('There is no tracks to play, use `streamUrl` option or `load` method');
+  }
 
+  if (this._clientId) {
     src = _appendQueryParam(src, 'client_id', this._clientId);
+  }
 
-    if (src !== this.audio.src) {
-        this.audio.src = src;
-    }
+  if (src !== this.audio.src) {
+    this.audio.src = src;
+  }
 
-    this.playing = src;
+  this.playing = src;
 
-    this.audio.play();
+  this.audio.play();
 };
 
 SoundCloud.prototype.pause = function () {
-    this.audio.pause();
-    this.playing = false;
+  this.audio.pause();
+  this.playing = false;
 };
 
 SoundCloud.prototype.stop = function () {
-    this.audio.pause();
-    this.audio.currentTime = 0;
-    this.playing = false;
+  this.audio.pause();
+  this.audio.currentTime = 0;
+  this.playing = false;
 };
 
 SoundCloud.prototype.next = function () {
-    var tracksLength = this._playlist.tracks.length;
-    if (this._playlistIndex >= tracksLength - 1) {
-        return;
-    }
-    if (this._playlist && tracksLength) {
-        this.play({playlistIndex: ++this._playlistIndex});
-    }
+  var tracksLength = this._playlist.tracks.length;
+
+  if (this._playlistIndex >= tracksLength - 1) {
+    return;
+  }
+
+  if (this._playlist && tracksLength) {
+    this.play({playlistIndex: ++this._playlistIndex});
+  }
 };
 
 SoundCloud.prototype.previous = function () {
-    if (this._playlistIndex <= 0) {
-        return;
-    }
-    if (this._playlist && this._playlist.tracks.length) {
-        this.play({playlistIndex: --this._playlistIndex});
-    }
+  if (this._playlistIndex <= 0) {
+    return;
+  }
+
+  if (this._playlist && this._playlist.tracks.length) {
+    this.play({playlistIndex: --this._playlistIndex});
+  }
 };
 
 SoundCloud.prototype.seek = function (e) {
-    if (!this.audio.readyState) {
-        return false;
-    }
-    var percent = e.offsetX / e.target.offsetWidth || (e.layerX - e.target.offsetLeft) / e.target.offsetWidth;
-    this.audio.currentTime = percent * (this.audio.duration || 0);
+  if (!this.audio.readyState) {
+    return false;
+  }
+
+  var percent = e.offsetX / e.target.offsetWidth || (e.layerX - e.target.offsetLeft) / e.target.offsetWidth;
+
+  this.audio.currentTime = percent * (this.audio.duration || 0);
 };
 
 SoundCloud.prototype.cleanData = function () {
-    this._track = void 0;
-    this._playlist = void 0;
+  this._track = void 0;
+  this._playlist = void 0;
 };
 
 SoundCloud.prototype.setVolume = function (volumePercentage) {
-    if (!this.audio.readyState) {
-        return;
-    }
-    this.audio.volume = volumePercentage;
-}
+  if (!this.audio.readyState) {
+    return;
+  }
+
+  this.audio.volume = volumePercentage;
+};
 
 SoundCloud.prototype.setTime = function (seconds) {
-    if (!this.audio.readyState) {
-        return;
-    }
-    this.audio.currentTime = seconds;
-}
+  if (!this.audio.readyState) {
+    return;
+  }
+
+  this.audio.currentTime = seconds;
+};
 
 module.exports = SoundCloud;
 
-},{}]},{},[1])(1)
+},{}]},{},["/Users/dmitri/github/soundcloud-html5-audio"])("/Users/dmitri/github/soundcloud-html5-audio")
 });
